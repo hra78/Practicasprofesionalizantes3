7d94585d0a2e58e9d54a016226c406bd90cafdee
hra78 <hectorraulalvarez78@gmail.com>
2023-09-30 17:28:16 -0300

    array de objetos

--- repaso/array_objetos/main.js
+++ repaso/array_objetos/main.js
@@ -7,21 +7,21 @@ const alumnos = [
             celular:2994200136,
             fijo:2994775656
         }
-        
     },
-{
-    legajo:544,
-    usuario:'santiagov' ,
-    password: '321cba' ,
-    telefonos:{
-        celular:2994200136,
-        fijo:2994775656
-}
-}
-
-    ]
-
-alumnos.forEach(Element => {
+    {
+        legajo:544,
+        usuario:'santiagov' ,
+        password: '321cba' ,
+        telefonos:{
+            celular:2994200136,
+            fijo:2994775656
+        }
+    },
+]//aca arriba cargamos dos elementos al arreglo alumnos
+//se indico cada elemento separado de coma con los datos pertenecientes a cada elemento entre llaves
+alumnos.forEach(element => {//el comando for each de javascrpit recorre el arreglo ejecutando las funciones que indiquemos
     console.log(element.usuario)
+    console.log(element.legajo)//muestra en consola otrA propiedad del objeto
+    document.write(element.telefonos.fijo)// muestra en pantalla la propiedad del objeto
 });
 

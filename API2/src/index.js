2f70486cd765b4cf9fd652e1f4663c95b4ca5181
hra78 <hectorraulalvarez78@gmail.com>
2023-10-09 19:22:02 -0300

    delfo corrigio ondemon y un punto y coma para que funcione el comando npm run dev

--- API2/src/index.js
+++ API2/src/index.js
@@ -9,11 +9,11 @@ const server = express()
 //ponemos en marcha el servidor
 //server.listen(server.get('port'))
 
-console.log('servidor corriendo en puerto: ',server.get('port'))
-
 // especificamos puerto donde correra nuestro server 
 server.set('port',3000)
 
 //ponemos en marcha nuestro servidor
 // en el puerto 3000
 server.listen(server.get('port'))
+
+console.log('servidor corriendo en puerto: ',server.get('port'))

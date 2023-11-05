3495c1b3840bdb04660f70421030fef67127fd88
hra78 <hectorraulalvarez78@gmail.com>
2023-11-05 16:39:54 -0300

    vue2-proyecto-prueba2.mp4

---
+++ vue2/prueba2/src/views/Noticias.vue
@@ -0,0 +1,17 @@
+<template>
+    
+    <NoticiaContenido/>
+
+</template>
+
+<script>
+
+import NoticiaContenido from '@/components/NoticiaContenido.vue';
+
+export default {
+    name:'noticiasView',
+    components:{
+        NoticiaContenido
+    }
+}
+</script>

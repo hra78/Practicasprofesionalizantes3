2d1f980ea98c5fb12a70e3e305c64cacbfa6757a
hra78 <hectorraulalvarez78@gmail.com>
2023-11-06 15:04:10 -0300

    Directiva v-for generacion de tabla clase 16

--- vue2/prueba2/src/components/FormCliente.vue
+++ vue2/prueba2/src/components/FormCliente.vue
@@ -10,6 +10,31 @@
              <div>
              <button @click="mostrar()" class="btn btn-success">Aceptar</button>
              </div>
+
+             <div class="col-lg-8">
+                <h4>{{titulo}}</h4>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>N°</th>
+                            <th>Nombre</th>
+                            <th>Usuario</th>
+                            <th>Correo Electronico</th>
+                            <th>Website</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr v-for="(u, index) of list_usuarios" v-bind:key="index">
+                            <td>{{index}}</td>
+                            <td>{{u.name}}</td>
+                            <td>{{u.username}}</td>
+                            <td>{{u.email}}</td>
+                            <td>{{u.website}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+             </div>
     </div>
 </template>
 <script>
@@ -21,23 +46,34 @@ export default {
         //sector de variables
         let nombre = ref('')
         let mensaje = ref('Probando el uso de VUE')
+        let list_usuarios = ref([])
+        let titulo = ref('Lista de usuarios')
         //sector de funciones
         function mostrar(){
-            alert(nombre.value)
+            //alert(nombre.value)
         }
         function mostrar_mensaje(){
-            alert(mensaje.value)
+            //alert(mensaje.value)
+        }
+
+        async function consumir_api(){
+            const usuarios = await fetch('https://jsonplaceholder.typicode.com/users')
+            list_usuarios.value = await usuarios.json()
         }
 
         return{
             nombre,
             mostrar,
             mensaje,
-            mostrar_mensaje
+            mostrar_mensaje,
+            consumir_api,
+            list_usuarios,
+            titulo
         }
     },
     created(){
         this.mostrar_mensaje()
+        this.consumir_api()
     }
 }
 </script>

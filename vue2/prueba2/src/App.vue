1d86962d3e90e4cd6f7c36c5c37892e6e4c01193
hra78 <hectorraulalvarez78@gmail.com>
2023-11-06 14:03:59 -0300

    screen-capture directiva v-for

--- vue2/prueba2/src/App.vue
+++ vue2/prueba2/src/App.vue
@@ -1,11 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/nuestrasnoticias">Noticias</router-link> |
-    <router-link to="/clientes">clientes</router-link>
-  </nav>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
    <div class="container-fluid">
      <a class="navbar-brand" href="#">Prueba2</a>
      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,7 +15,9 @@
      </div>
    </div>
   </nav>
-  <router-view/>
+  <div class="container mt-3">
+    <router-view/>
+  </div>
 </template>
 
 <style>
--- vue2/prueba2/src/components/FormCliente.vue
+++ vue2/prueba2/src/components/FormCliente.vue
@@ -1,14 +1,43 @@
 <template>
-    <h1>Probando componente</h1>
-    <input type="text">
-    <button>Aceptar</button>
+    <div class="row">
+        <div class="col-lg-4">
+            <h3>Probando componente</h3>
+            <div class="mb-3">
+                 <label class="form-label">Nombre</label>
+                 <input v-model="nombre" class="form-control" type="text">
+            </div>
+        </div>
+             <div>
+             <button @click="mostrar()" class="btn btn-success">Aceptar</button>
+             </div>
+    </div>
 </template>
 <script>
 /* eslint-disable */
+import { ref } from 'vue'
 export default {
     name:'FormCliente',
     setup() {
+        //sector de variables
+        let nombre = ref('')
+        let mensaje = ref('Probando el uso de VUE')
+        //sector de funciones
+        function mostrar(){
+            alert(nombre.value)
+        }
+        function mostrar_mensaje(){
+            alert(mensaje.value)
+        }
 
+        return{
+            nombre,
+            mostrar,
+            mensaje,
+            mostrar_mensaje
+        }
+    },
+    created(){
+        this.mostrar_mensaje()
     }
 }
 </script>
